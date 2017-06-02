09764ada8265a1805e81e55bfa11519d6044286d
Elizabeth2909 <elizabeth.diaz@blackinntech.com>
2017-06-02 10:28:24 -0500

    genera cajas

---
+++ generaCajas.php
@@ -0,0 +1,73 @@
+<?php 
+
+if(isset($_POST['Guardar'])){
+	$instruccion=$_POST['txt_instrucciones'];
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+	<title>Cajas dinamicas</title>
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+</head>
+<body>
+
+<form name="form1" action="" method="POST">
+	<p>	Instrucciones: <input type="text" name="txt_instrucciones" id="txt_instruccion"></p>
+    <!--<button type="button" form="form1" onclick="addInstrucciones()" value="Agregar">Agregar</button><br>-->
+    <label>Cuantas cajas requiere? </label><select name="select1" onchange="createTexts(this)">
+    <option value="" selected="selected">          </option>
+        <?php
+        for($i=2; $i<=15;$i++){
+        echo "<option value='$i'>$i</option>";
+        }
+        ?> 
+    </select><br>
+    <div></div>
+    <br>
+    <button type="submit" form="form1"  value="Guardar">Guardar</button>
+    <button type="submit" form="form1"  onclick="formReset()" value="NuevaPreg">Agregar serie de cajas</button>
+</form>
+
+	<script>
+	function formReset(){
+		document.getElementById("form1").reset();
+	}
+
+
+
+	function addInstrucciones(){
+	
+		var textBoxTexto = document.getElementById("txt_preg").value;
+		var etiqueta=document.createElement("label");	
+		var texth1 = document.createTextNode(textBoxTexto);
+		etiqueta.appendChild(texth1);
+		document.body.appendChild(etiqueta);	
+		}
+
+
+		idtxt=0;
+		
+			function createTexts(sel) {
+				
+				var num = sel.text;
+				if( !num ) num = sel.options[sel.selectedIndex].text;
+				if( !num ) return;
+				var html="<br><input type='text' name='caja_de_texto[]' id="+idtxt+"/><br>";
+				num = parseInt( num );
+				num = isNaN(num) ? 0 : num;
+				sel.value = num;
+				var dest = sel.parentNode.getElementsByTagName("div")[0];
+				dest.innerHTML = '';
+				for( i = 0; i < num; i++ ) {
+					dest.innerHTML += html;
+				}
+				idtxt++;
+
+}
+	</script>
+</body>
+</html>
